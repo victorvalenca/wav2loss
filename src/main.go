5cf7eedac9049e13ab0187da6661fb44571db58f
victorvalenca <victorvalenca@gmail.com>
2018-05-03 14:19:30 -0400

    Detect environment variables in YAML

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"runtime"
 
@@ -20,7 +21,9 @@ type Configuration struct {
 }
 
 var (
-	conf *Configuration
+	conf    *Configuration
+	opusBin string
+	lameBin string
 )
 
 func readConf() *Configuration {
@@ -55,24 +58,51 @@ func systemCheck() {
 	fmt.Printf("I am running on: %s\n", osVersion)
 	fmt.Printf("%+v\n", conf)
 
-	opusBin, err := exec.LookPath("opusenc")
+	res, err := exec.LookPath("opusenc")
 	if err != nil {
 		fmt.Printf("Could not find opusenc in PATH\n")
 	}
-	lameBin, err := exec.LookPath("lame")
+	opusBin = res
+
+	res2, err := exec.LookPath("lame")
 	if err != nil {
 		fmt.Printf("Could not find LAME in PATH\n")
 	}
+	lameBin = res2
+}
 
-	_ = opusBin
-	_ = lameBin
-	fmt.Printf("opus:\t%s\nLAME:\t%s\n", opusBin, lameBin)
+// Configuration check routine
+func checkConf() bool {
 
+	// TODO: Parse envvars to full path
+	switch conf.RecordDirectory[0] {
+	case '$':
+		fmt.Printf("I am dealing with a UNIX-style envvar, use full path please\n")
+		return false
+	case '%':
+		fmt.Printf("I am dealing with a Windows-style envvar, use full path please\n")
+		return false
+	default:
+		return true
+	}
 }
 
 // Main program
 func main() {
-	// TODO: Check config file parameters
 	// TODO: Test executables
+
+	fmt.Printf("opus:\t%s\nLAME:\t%s\n", opusBin, lameBin)
+	opusTest := exec.Command(opusBin, "--version")
+	lameTest := exec.Command(lameBin, "--version")
+
+	opusTest.Stdout, lameTest.Stdout = os.Stdout, os.Stdout
+	opusTest.Stderr, lameTest.Stderr = os.Stderr, os.Stderr
+
+	pass := checkConf()
+	if !pass {
+		fmt.Printf("Configuration check failed\n")
+	}
+	// opusTest.Run()
+	// lameTest.Run()
 
 }
